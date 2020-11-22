c2adfc8e74335a321f3f3bd5bbf2c758fd8e94ec
romaditro <sr.rdiazt@gmail.com>
2020-11-21 21:22:06 -0300

    ultimos cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
 
+
+    $(".nav-item a.nav-link, .navbar-brand, .btn-work").click(function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var gato = this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function() {
+                window.location.hash = gato;
+            });
+        }
+    });
+
+
+    //Popovers
+    $(function() {
+        $('[data-toggle="popover"]').popover()
+    })
 });
